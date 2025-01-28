47033b9e02926df964a4518aa5617a40c32901d0
unknown <elishamedenoukoume@gmail.com>
2025-01-28 00:19:01 -1100

    Fonction pour trouver le nombre de caractères du texte

---
+++ exo_01.dart
@@ -0,0 +1,26 @@
+void main() {
+    String letter = '''Cher agent X,
+
+
+
+J'ai aperçu le chat noir    au point de rendez-vous 7. L'agent Johnson était 
+
+également présent.    Le code secret 42 a été activé.
+
+
+
+Notre agent de liaison vous contactera à 23h.
+
+
+
+    Cordialement''';
+    print (letter);
+  protocoleAlpha (letter);  
+} 
+
+ void protocoleAlpha (String message){
+    int nbrDeCaracteres = message.length;
+    print("Le nombre de caratères est égale à $nbrDeCaracteres.");
+    }
+  
+ 
