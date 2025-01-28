ee4533348c214d760763dfa41d2ba52e062c288a
unknown <elishamedenoukoume@gmail.com>
2025-01-28 02:44:28 -1100

    Fonction pour vérifier le protocole Gamma.

--- exo_01.dart
+++ exo_01.dart
@@ -48,6 +48,42 @@ Notre agent de liaison vous contactera à 23h.
   });
   print(message);
 }
+
+
+void protocoleGamma () {
+ String letter = '''Cher agent X,
+
+
+
+J'ai aperçu le chat noir    au point de rendez-vous 7. L'agent Johnson était 
+
+également présent.    Le code secret 42 a été activé.
+
+
+
+Notre agent de liaison vous contactera à 23h.
+
+
+
+    Cordialement''';
+  String prefix = "Cher";
+
+  if (letter.startsWith(prefix)) {
+    print("La chaîne commence par '$prefix'.");
+  } else {
+    print("La chaîne ne commence pas par '$prefix'.");
+  }
+  String suffixe = "Cordialement";
+
+  if (letter.endsWith(suffixe)) {
+    print("La phrase se termine par '$suffixe'.");
+  } else {
+    print("La phrase ne se termine pas par '$suffixe'.");
+  }
+  print(letter.replaceAll(RegExp(r"\s+"), " ").trim());
+print(letter.trim());
+}
+
   
 
  
